285ed015bd15d4eee174de6c5e4ba9ab97652162
Meya Build <build@meya.ai>
2026-05-21 17:24:18 +0000

    Snapshot v2.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="meya-sdk",
     packages=find_namespace_packages(),
-    version="2.9.0",
+    version="2.9.1",
     description="Meya SDK",
     url="https://meya.ai",
     author="Meya",
